2cfc4d2af0506a9343ae5b043e3e74228ca9d51f
Corey Gallagher <coreygallagher1194@gmail.com>
2022-03-30 21:40:01 -0500

    connected AWS postgres DB

--- Untether/settings.py
+++ Untether/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'Untether.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'untether.c2kxaqhpw6xu.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
